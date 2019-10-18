214d8302fbace20cbeadd30b36fedd4555f631fe
othonrm <othon200@gmail.com>
2019-10-17 22:23:25 -0300

    Ultimo dia completo

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 
 import api from '../../services/api';
 
@@ -9,6 +9,22 @@ import './styles.css';
 export default function Dashboard({ history }) {
 
     const [spots, setSpots] = useState(null);
+
+    const [requests, setRequests] = useState([]);
+
+    const user_id = localStorage.getItem('user');
+
+    const socket = useMemo(() => socketio('http://localhost:3333', {
+        query: { user_id },
+    }), [user_id]);
+
+    useEffect(() => {
+
+        socket.on('booking_request', data => {
+            setRequests([ ...requests, data]);
+        });
+
+    }, [requests, socket]);
 
     useEffect(() => {
 
@@ -27,18 +43,50 @@ export default function Dashboard({ history }) {
                 headers: { user_id },
             });
 
-            console.log(response.data);
+            console.log("Spots encontrados:", response.data);
 
             setSpots(response.data);
         }
 
         loadSpots();
 
-    }, []);
+    }, [history]);
+
+    async function handleAccept(booking_id) {
+
+        await api.post(`/bookings/${booking_id}/approvals`);
+
+        setRequests(requests.filter(request => request._id !== booking_id));
+    }
+
+    async function handleReject(booking_id) {
+
+        await api.post(`/bookings/${booking_id}/rejections`);
+
+        setRequests(requests.filter(request => request._id !== booking_id));
+    }
 
 
     return (
         <>
+            <ul className="notifications">
+                {
+                    requests.map(request => (
+                        <li key={request._id}>
+                            <p>
+                                <strong>{request.user.email}</strong> está solicitando uma reserva em <strong>{request.spot.company}</strong> para a data: <strong>{request.date}</strong>
+                            </p>
+                            <button className="accept" onClick={() => handleAccept(request._id)}>
+                                Aceitar
+                            </button>
+                            <button className="reject" onClick={() => handleReject(request._id)}>
+                                Rejeitar
+                            </button>
+                        </li>
+                        )
+                    )
+                }
+            </ul>
             <h1 className="title">Dashboard</h1>
 
             {
