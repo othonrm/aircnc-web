37daeb4f437973d40c4a001cb844cb14cc0789ca
othonrm <othon200@gmail.com>
2019-10-18 11:38:07 -0300

    Static build teste

---
+++ build/precache-manifest.5c6c4e32fc32311fb96be7e64591c7ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af23b4522215ca44c84ca0d43af926ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a05d76e90cfe6ce8180",
+    "url": "/static/css/main.b2d0e2b6.chunk.css"
+  },
+  {
+    "revision": "aa24c2494b6e530b95a4",
+    "url": "/static/js/2.109aca1a.chunk.js"
+  },
+  {
+    "revision": "9a05d76e90cfe6ce8180",
+    "url": "/static/js/main.0209f73b.chunk.js"
+  },
+  {
+    "revision": "8a2c7ed04057643e3989",
+    "url": "/static/js/runtime-main.4a9ee956.js"
+  },
+  {
+    "revision": "83252e0afaf5f134063345abd6ac8fd7",
+    "url": "/static/media/background.83252e0a.jpg"
+  },
+  {
+    "revision": "6a6f4b6ebebfabdc283a3a6b417282a0",
+    "url": "/static/media/camera.6a6f4b6e.svg"
+  },
+  {
+    "revision": "740528028131fdef515dcb930373ccf6",
+    "url": "/static/media/logo.74052802.svg"
+  }
+]);
